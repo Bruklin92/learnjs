2b532cbd29b7a5bcf87f864f6dc74ad74a1b7040
Bruklin92 <meetajudiya93@gmail.com>
2024-12-14 17:43:54 +0530

    kg

--- js/arraytask.js
+++ js/arraytask.js
@@ -1,4 +1,4 @@
-let a = [10, 20, 30, 40, 50];
+// let a = [10, 20, 30, 40, 50];
 
 // 1.    Finding the maximum element in an array.
 
@@ -33,3 +33,88 @@ let a = [10, 20, 30, 40, 50];
 
 // let ans = a.reverse();
 // console.log(ans);
+
+// 6.    Finding the sum of all elements in an array.
+
+// let ans = a.reduce((acc, v, i) => acc+v,0);
+// console.log(ans);
+
+// 7.    Finding the average of all elements in an array.
+
+// let ans=a.reduce((acc,v,i) => acc+v, 0);
+// console.log(ans/a.length);
+
+// 8.    Checking if an array contains a specific element.
+
+//  let a=[10,20,30,40,50,50,60];
+// let ans=a.includes(50);
+// console.log(ans);
+
+// 9.    Removing duplicates from an array.
+
+    // let emptyArray=[];
+    // for(let i=0; i<a.length;i++){
+    //     if(!emptyArray.includes(a[i])){
+    //         emptyArray.push(a[i])
+    //     }
+    // }
+    // console.log(emptyArray);
+
+//---------------------------------------HOME---------------------------------------------//
+
+// 10.    Merging two arrays into a new array.
+
+// let a2=[1,2,3,4];
+// let ans=a.concat(a2);
+// console.log(ans);
+
+
+
+// 11.    Splitting an array into two arrays based on a condition.
+
+let arr=[1,2,3,4,5,6,7,8,9,10];
+let a1 = [];
+let a2 = [];
+let ans = arr.reduce((even,odd,num) => {
+    if(num%2==0){
+        even.push(num);
+    } else {
+        odd.push(num);
+    }
+});
+console.log(arr);
+
+
+
+// 12.    Rotating an array by a given number of positions.
+
+
+// 13.    Finding the second largest element in an array.
+
+// let a=[10,20,30,40,50]
+// let ans= a.sort((a,b) => b-a);
+// console.log(a[1]);
+
+// let a = [1, 2, 3, 4];
+// let a2 = [1, 2, 3, 4];
+// let ans = a.length === a2.length && a.every((value, index) => value === a2[index]);
+// console.log(ans);
+
+// 17.    Finding the index of the first occurrence of an element in an array.
+
+// let a = [10, 20, 30, 40, 50, 30];
+// let ans = a.indexOf(30);
+// console.log(ans);  
+
+// 18.    Finding the index of the last occurrence of an element in an array.
+
+// let a = [10, 20, 30, 40, 50, 30];
+// let ans = a.lastIndexOf(30);
+// console.log(ans);  
+
+// 24.    Finding the first three maximum number's sum an array.
+
+// let a = [10, 20, 40, 30, 50, 40];
+// a.sort((a, b) => b - a);
+// let sum = a[0] + a[1] + a[2];
+// console.log(sum);
---
+++ js/arrtest.js
@@ -0,0 +1,120 @@
+// let arr1 = [10,8,61,31,5];
+
+// 1. Change 61 to 20.
+
+// arr1[2] = 20;
+// console.log(arr1);
+
+
+// 2. Delete 8
+
+// let ans = arr1.splice(1,1);
+// console.log(arr1);
+
+
+// 3. Add 20,30,40 after 10
+
+// let ans = arr1.splice(1, 0, 20, 30, 40);
+// console.log(arr1);
+
+
+// 4. Find index of 5
+
+// let ans = arr1.indexOf(5);
+// console.log(ans);
+
+
+// 5. Search 99 is available or not
+
+// let ans = arr1.includes(99);
+// console.log(ans);
+
+//---------------Q2--------------------//
+
+// let arr2 = [10,20,30,40,50];
+// let arr3 = [6,9,80];
+
+// 1. Combine arr2 and arr3
+
+// let ans = arr2.concat(arr3);
+// console.log(ans);
+
+
+// 2. Get 20, 30, 40 as array from arr2
+
+// let ans = arr2.slice(1,4);
+// console.log(ans);
+
+
+// 3. Give sum of all element of arr3 using reduce function
+
+// let ans = arr3.reduce((acc, v, i) => acc+v, 0);
+// console.log(ans);
+
+
+// 4. Check all element is greater than 10 or not from arr3
+
+// let ans = arr3.every((i,v) => v > 10);
+// console.log(ans);
+
+
+// 5. Sort arr3 in descending order.
+
+// let ans = arr3.sort((a,b) => b-a);
+// console.log(ans);
+
+//---------------Q3--------------------//
+
+// let arr = [10, 20, 30, 40, 50];
+
+// 1. Removing duplicates from an array.
+
+// let arr = [10, 20, 20, 30, 30, 40, 50];
+
+// let arr1=[];
+//     for(let i=0; i<arr.length; i++){
+//         if(!arr1.includes(arr[i])){
+//             arr1.push(arr[i])
+//         }
+//     }
+// console.log(arr1);
+
+
+// 3. Checking if an array is sorted in ascending order.
+
+// let a1 = arr.sort((a,b) => a-b);
+// console.log(a1);
+
+
+// 4. Finding the third largest element in an array.
+
+// let ans = arr.sort((a,b) => b-a);
+// console.log(ans[2]);
+
+
+// 5. Finding the first three maximum number's sum an array.
+
+// arr.sort((a,b) => b-a);
+// let max = arr.slice(0, 3);
+// let ans = max.reduce((acc, v, i) => acc+v,0);
+// console.log(ans);
+
+
+
+
+// Q-4 Create a todo list that add, display, sort a todo.
+
+
+function handlesubmit() {
+    event.preventDefault();
+
+    let print;
+    let text = document.getElementById("text").value;
+    console.log(text);
+    for (let i=0; i<text[i]; i++) {
+        text.push("text");
+    }
+    print+=
+     document.getElementById("disp").innerHTML=text;
+
+}
